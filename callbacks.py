221b4f570f25b352d19302a8cf492c54955ed43d
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 01:15:10 +0530

    Delete callbacks.py

--- callbacks.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from storage import database as db
-from engine import tasks, win
-
-def handle_callback(update: Update, context: CallbackContext):
-    ...
