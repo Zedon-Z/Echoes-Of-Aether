3b18c38026753076fff6de55a91e5bef30ec7020
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 12:23:07 +0530

    Delete dm.py

--- dm.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from storage import database as db
-from engine import roles, tasks, inventory
-
-def handle_dm(update: Update, context: CallbackContext):
-    ...
