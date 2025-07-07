df74ed93f277287d4271d6f540364703bea054f2
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 12:21:27 +0530

    Update commands.py

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,1 +1,7 @@
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CallbackContext
+from storage import database as db
+from engine import phases
 
+def start_game(update: Update, context: CallbackContext):
+    ...
