33100f82b276ce9d6295af313b464f7cb19fa626
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 18:43:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
-import os
-import sys
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+import os
+from threading import Thread
 from handlers import commands, callbacks, game, dm
 from config import TOKEN
 
-print("Python version:", sys.version)
-
-def main():
+def run_bot():
     updater = Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Register handlers
+    dp.add_handler(CommandHandler("start", commands.start))
     dp.add_handler(CommandHandler("startgame", commands.start_game))
     dp.add_handler(CommandHandler("join", commands.join_game))
     dp.add_handler(CommandHandler("vote", commands.vote))
@@ -18,9 +16,8 @@ def main():
     dp.add_handler(CallbackQueryHandler(callbacks.handle_callback))
     dp.add_handler(MessageHandler(Filters.private & Filters.text, dm.handle_dm))
 
-    # Webhook setup for Render
-    PORT = int(os.environ.get('PORT', '8443'))
-    APP_URL = os.environ.get("APP_URL")  # You must set this in Render env vars
+    PORT = int(os.environ.get("PORT", 8443))
+    APP_URL = os.environ.get("APP_URL")
 
     updater.start_webhook(
         listen="0.0.0.0",
@@ -31,5 +28,6 @@ def main():
 
     updater.idle()
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    Thread(target=run_bot).start()
+    import server  # Runs the Flask app to expose port
