6b00c51eaf3e41d9c4e082df648571cd5f76b523
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 12:22:53 +0530

    Delete game.py

--- game.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from engine import phases
-from storage import database as db
-
-def phase(update: Update, context: CallbackContext):
-    ...
