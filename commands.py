15ae00c68f06e6ebaad4502ad84b7393e99ebb3f
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 12:23:20 +0530

    Delete commands.py

--- commands.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import CallbackContext
-from storage import database as db
-from engine import phases
-
-def start_game(update: Update, context: CallbackContext):
-    ...
