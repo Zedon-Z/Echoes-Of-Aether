53938f93b64638c312368aee16b0453cdabe3798
Zedon-Z <lrxzedon@gmail.com>
2025-07-07 20:38:21 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+from storage import authorized
+
+def group_allowed(func):
+    @wraps(func)
+    def wrapped(update: Update, context: CallbackContext, *args, **kwargs):
+        chat_id = update.effective_chat.id
+        if not authorized.is_group_authorized(chat_id):
+            update.effective_message.reply_text("🔒 This group is not authorized to use the bot.")
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapped
